b0b70a6b6b072300009497c5d4a06dc7fe2950cf
Oleg Rakovitch <devgeslot@gmail.com>
2020-06-29 10:29:01 -0400

    Changed map component to use svg polygons instead of area map

--- src/directives/spy.directive.ts
+++ src/directives/spy.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, OnInit, OnDestroy, HostListener } from '@angular/core';
 
-import { SpotsService } from '../services';
+import { SpotsService, LocationSelectorService } from '../services';
 import { waitUntil } from '../utils';
 
 declare var $: any;
@@ -10,36 +10,37 @@ interface Image {
 }
 
 @Directive({selector: '[vSpy]'})
-export class SpyDirective implements OnInit, OnDestroy {
+export class SpyDirective {
   private image: Image;
 
   constructor(
-    private spotsService: SpotsService
-  ) { }
+    private spotsService: SpotsService,
+    maps: LocationSelectorService
+  ) {
+    maps.location.subscribe(this.provideRatio.bind(this));
+  }
 
-  ngOnInit(): void {
-    this.image = $('div.map img.image')[0];
+  @HostListener('window:resize')
+  onResize(): void {
     this.provideRatio();
   }
 
   private async provideRatio(): Promise<void> {
     await waitUntil(this.imageRendered.bind(this));
+    this.image = this.getImage();
     this.spotsService.setRatio(this.calculateRatio());
   }
 
-  @HostListener('window:resize')
-  onResize(): void {
-    this.spotsService.setRatio(this.calculateRatio());
+  private getImage(): Image {
+    return $('div.map img.image')[0];
   }
 
   private imageRendered(): boolean {
-    return !!this.image.width && !!this.image.naturalWidth;
+    const image = this.getImage();
+    return !!image && !!image.width && !!image.naturalWidth;
   }
 
   private calculateRatio(): number {
     return this.image.width / this.image.naturalWidth;
-  }
-
-  ngOnDestroy(): void {
   }
 }
